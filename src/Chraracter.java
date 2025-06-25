c7f28ae01bd584003757c1e253fdc442cf1db884
muramurao <241059@std.hi-joho.ac.jp>
2025-06-25 15:04:53 +0900

    Initial commit

---
+++ src/Chraracter.java
@@ -0,0 +1,13 @@
+public abstract class Chraracter implements Creature {
+    String name;
+    int hp;
+    public Character(String input_name){
+        this.name = input_name;
+        this.hp = 100;
+    }
+    public abstract void attack();
+    public abstract void skill(Hero hero);
+    public void protect(){
+        System.out.println(this.name +"は、まもりのたいせいにはいった!");
+    }
+}
---
+++ src/Creature.java
@@ -0,0 +1,3 @@
+public  interface Creature {
+    void greething();
+    void escape();
